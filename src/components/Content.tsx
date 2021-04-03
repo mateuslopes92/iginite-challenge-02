5279dbfd70426b1bab72f05ddaf64f6432bc6fc8
Mateus dos Santos Lopes <mateuslopes92@gmail.com>
2021-04-03 14:40:06 -0300

    Finished project

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,43 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "./MovieCard";
+
+import '../styles/content.scss';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentProps {
+  movieList: MovieProps[],
+  selectedGenre: GenreResponseProps
+}
+
+export function Content({movieList, selectedGenre}: ContentProps) {
+  return (
+    <>
+      <header>
+        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movieList.map(movie => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </>
+  )
 }
